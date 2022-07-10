690ab5581ffa0dfe7d36da96f105c6237d64056a
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-07-09 22:39:00 -0300

    Creating templates files and folders

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import path
 
 
 
 def home(request):
-    return HttpResponse("HOME")
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
