2dfe7c8cc825349afad3c70a6018292d01189ff6
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-07-16 15:00:36 -0300

    Configure views data models

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by("-id")
     return render(
         request,
         "recipes/pages/home.html",
-        context={"recipes": [make_recipe() for _ in range(10)]},
+        context={"recipes": recipes},
+    )
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
+    print(recipes)
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={"recipes": recipes},
     )
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(id=id).first()
     return render(
         request,
         "recipes/pages/recipe-view.html",
-        context={"recipe": make_recipe(), "is_detail_page": True},
+        context={"recipe": recipe, "is_detail_page": True},
     )
