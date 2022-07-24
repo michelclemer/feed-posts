7788006f6456f1b5f89b3c5f3ed1a23ee1ccd08a
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-07-23 22:19:04 -0300

    Add title and error status

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.http import Http404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by("-id")
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
     return render(
         request,
         "recipes/pages/home.html",
@@ -13,17 +14,25 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
-    print(recipes)
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by("-id")
+
+    if not recipes:
+        raise Http404("Not Found!!")
+
     return render(
         request,
-        "recipes/pages/home.html",
-        context={"recipes": recipes},
+        "recipes/pages/category.html",
+        context={
+            "recipes": recipes,
+            "title": f"{recipes.last().category.name}  - Category",
+        },
     )
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(id=id).first()
+    recipe = Recipe.objects.filter(id=id, is_published=True).first()
     return render(
         request,
         "recipes/pages/recipe-view.html",
